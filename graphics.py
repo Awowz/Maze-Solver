94ee2c720bd8e516297997de3f4977f9775f99f9
Awowz <tonygriffith75@gmail.com>
2025-04-10 20:17:53 -0500

    added draw_move function to graphics.py

--- graphics.py
+++ graphics.py
@@ -36,4 +36,40 @@ class Line:
         self.pos2 = pos2
 
     def draw(self, canvas, fill_color):
-        canvas.create_line(self.pos1.x, self.pos1.y, self.pos2.x, self.pos2.y, fill=fill_color, width=5)
+        canvas.create_line(self.pos1.x, self.pos1.y, self.pos2.x, self.pos2.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, window, top_left_pos, bottom_right_pos, left=True,right=True,top=True,bottom=True):
+        self.has_left_wall = left
+        self.has_right_wall = right
+        self.has_top_wall = top
+        self.has_bottom_wall = bottom
+        self._top_left_pos = top_left_pos
+        self._bottom_right_pos = bottom_right_pos
+        self._win = window
+
+    def draw(self):
+        if self.has_top_wall:
+            top = Line(Point(self._top_left_pos.x, self._top_left_pos.y), Point(self._bottom_right_pos.x, self._top_left_pos.y))
+            self._win.draw_line(top)
+        if self.has_bottom_wall:
+            bottom = Line(Point(self._top_left_pos.x, self._bottom_right_pos.y), Point(self._bottom_right_pos.x, self._bottom_right_pos.y))
+            self._win.draw_line(bottom)
+        if self.has_right_wall:
+            right = Line(Point(self._bottom_right_pos.x, self._top_left_pos.y), Point(self._bottom_right_pos.x, self._bottom_right_pos.y))
+            self._win.draw_line(right)
+        if self.has_left_wall:
+            left = Line(Point(self._top_left_pos.x, self._top_left_pos.y), Point(self._top_left_pos.x, self._bottom_right_pos.y))
+            self._win.draw_line(left)
+        
+    def get_center(self) -> Point:
+        distance_x = abs(self._bottom_right_pos.x - self._top_left_pos.x) // 2
+        distance_y = abs(self._bottom_right_pos.y - self._top_left_pos.y) // 2
+        return Point(self._top_left_pos.x + distance_x, self._top_left_pos.y + distance_y)
+        
+    def draw_move(self, to_cell, undo=False):
+        line_color = "black"
+        if undo:
+            line_color = "red"
+        line_connection = Line(self.get_center(), to_cell.get_center())
+        self._win.draw_line(line_connection, line_color)
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line, Point, Cell
 
 def main():
     win = Window(800, 600)
-    temp_line = Line(Point(50,35), Point(300,300))
-    win.draw_line(temp_line, "red")
+    cell_list = []
+    cell_list.append(Cell(win,Point(10,10), Point(40,40)))
+    cell_list.append(Cell(win,Point(10,50), Point(40,80), bottom=False,top=False))
+    cell_list.append(Cell(win,Point(50,10), Point(80,40), left=False))
+    cell_list.append(Cell(win,Point(90,10), Point(120,40), right=False))
+    cell_list.append(Cell(win,Point(130,10), Point(160,40), top=False))
+    cell_list.append(Cell(win,Point(170,10), Point(200,40), bottom=False))
+    for x in cell_list:
+        x.draw()    
+    cell_list[0].draw_move(cell_list[1])
+    cell_list[0].draw_move(cell_list[2], True)
     win.wait_for_close()
 
 main()
